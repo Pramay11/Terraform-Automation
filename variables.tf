d2d6492b068d6221af1883f5255be4071c708a87
Pramay11 <pramaygawali@gmail.com>
2025-12-26 22:38:39 +0530

    Update default key_name for EC2 instance

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "MY-FIRST-EC2-INSTANCE" 
+    default     =  "MY-FIRST-EC2-MACHINE" 
 }
 
 variable "instance_type" { 
