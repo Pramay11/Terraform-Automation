b5dbbeb13d4d09034da7c208fbeb8fb6b1edb2db
Pramay11 <pramaygawali@gmail.com>
2025-12-26 17:45:30 +0530

    added volume in 2nd ec2 instance

--- main.tf
+++ main.tf
@@ -77,11 +77,11 @@ resource "aws_instance" "mySecondInstance" {
 
   associate_public_ip_address = true
 
-  /*root_block_device {
+  root_block_device {
     volume_size = 20
     volume_type = "gp3"
     delete_on_termination = true
-  }*/
+  }
 
   tags = {
     Name = "${var.tag_name}-secondary"
