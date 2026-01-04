813f0a0aad6de8960342a2854bf2c20863851576
Pramay11 <pramaygawali@gmail.com>
2026-01-04 19:40:26 +0530

    added new ec2 instance

--- main.tf
+++ main.tf
@@ -68,3 +68,24 @@ tags= {
     Name = "my_elastic_ip"
   }
 }
+resource "aws_instance" "mySecondInstance" {
+  ami                    = var.ami_id
+  key_name               = var.key_name
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
+
+  root_block_device {
+    volume_size           = 20
+    volume_type           = "gp3"
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = "${var.tag_name}-2"
+  }
+}
+
+resource "aws_eip" "mySecondInstance" {
+  instance = aws_instance.mySecondInstance.id
+  tags = { Name = "my_elastic_ip_2" }
+}
