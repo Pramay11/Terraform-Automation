616fd122523c2571f40d1823f46c844ef7c56be1
Pramay11 <pramaygawali@gmail.com>
2026-01-04 21:31:30 +0530

    Change S3 bucket name for Terraform backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "project-sample-04012026"
+    bucket = "terraform-tfstate-bucket-04012026"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
