d744b5f1bac83c48c6d1a49d8759536a8b34b16e
Pramay11 <pramaygawali@gmail.com>
2026-01-04 21:42:41 +0530

    added new ec2 instance

--- main.tf
+++ main.tf
@@ -68,3 +68,31 @@ tags= {
     Name = "my_elastic_ip"
   }
 }
+
+# Second EC2 instance (added)
+resource "aws_instance" "mySecondInstance" {
+  ami           = var.ami_id
+  key_name      = var.key_name
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
+
+  # Set root volume size to 20 GB
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = "${var.tag_name}-2"
+  }
+}
+
+# Elastic IP for second instance
+resource "aws_eip" "mySecondInstance" {
+ // vpc      = true
+  instance = aws_instance.mySecondInstance.id
+  tags = {
+    Name = "my_second_elastic_ip"
+  }
+}
