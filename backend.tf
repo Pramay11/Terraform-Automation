31eaae88fc8de978b09d1c8510745e82d0bb4ace
Pramay11 <pramaygawali@gmail.com>
2026-01-04 19:22:31 +0530

    Update S3 bucket and DynamoDB table names

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-project-state"
+    bucket = "terraform-project-batch-30-04012026"
     key = "main"
     region = "ap-south-1"
-    dynamodb_table = "my-dynamodb-table-2612"
+    dynamodb_table = "my-dynamodb-table"
   }
 }
